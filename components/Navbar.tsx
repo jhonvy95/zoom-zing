923c35d1853c1c3c1af7072f7d13afa67fa31ba2
jhon varon <jhonvaronyate@hotmail.com>
2024-05-02 23:48:03 -0500

    Added navbar for mobile and desktop versions

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,25 @@
+import Image from "next/image";
+import Link from "next/link";
+import MovileNav from "./MovileNav";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+      <Link href={"/"} className="flex items-center gap-1">
+        <Image
+          src={"/icons/logo.svg"}
+          width={32}
+          height={32}
+          alt="zing logo"
+          className="max-sm:size-10"
+        />
+        <p className="text-[26px] font-extrabold text-white max-sm:hidden">ZoomZing</p>
+      </Link>
+
+      <div className="flex-between gap-5">
+        <MovileNav />
+      </div>
+    </nav>
+  );
 };
 export default Navbar;
