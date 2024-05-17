ed36c292e7b8990cd4e13c98c92d258a00ff09bc
jhon varon <jhonvaronyate@hotmail.com>
2024-05-17 15:04:14 -0500

    Setup meeting calls, meeting setup, and meeting room components.

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -18,7 +18,13 @@ const Meeting = ({ params: { id } }: { params: { id: string } }) => {
   return (
     <main className="h-screen w-full">
       <StreamCall call={call}>
-        <StreamTheme>{!isSetupComplete ? <MeetingSetup /> : <MeetingRoom />}</StreamTheme>
+        <StreamTheme>
+          {!isSetupComplete ? (
+            <MeetingSetup setIsSetupComplete={setIsSetupComplete} />
+          ) : (
+            <MeetingRoom />
+          )}
+        </StreamTheme>
       </StreamCall>
     </main>
   );
--- components/MeetingSetup.tsx
+++ components/MeetingSetup.tsx
@@ -1,7 +1,58 @@
-import React from "react";
+"use client";
 
-const MeetingSetup = () => {
-  return <div>MeetingSetup</div>;
+import { DeviceSettings, VideoPreview, useCall } from "@stream-io/video-react-sdk";
+import React, { useEffect, useState } from "react";
+import { Button } from "./ui/button";
+
+type MeetingSetupProps = {
+  setIsSetupComplete: (value: boolean) => void;
+};
+
+const MeetingSetup = ({ setIsSetupComplete }: MeetingSetupProps) => {
+  const [isMicCamtoggleOn, setIsMicCamtoggleOn] = useState(false);
+
+  const call = useCall();
+
+  if (!call) {
+    throw new Error("usecall must be used within StramCall component");
+  }
+
+  useEffect(() => {
+    if (isMicCamtoggleOn) {
+      call?.camera.disable();
+      call?.microphone.disable();
+    } else {
+      call?.camera.enable();
+      call?.microphone.enable();
+    }
+  }, [isMicCamtoggleOn, call?.camera, call?.microphone]);
+
+  return (
+    <div className="flex h-screen w-full flex-col items-center justify-center gap-3 text-white">
+      <h1 className="text-2xl font-bold">Setup</h1>
+      <VideoPreview />
+      <div className="flex h-6 items-center justify-center gap-3">
+        <label className="flex items-center justify-center gap-2 font-medium">
+          <input
+            type="checkbox"
+            checked={isMicCamtoggleOn}
+            onChange={(e) => setIsMicCamtoggleOn(e.target.checked)}
+          />
+          Join with mic and camera off
+        </label>
+        <DeviceSettings />
+      </div>
+      <Button
+        className="rounded-md bg-green-500 px-4 pt-2.5"
+        onClick={() => {
+          call.join();
+          setIsSetupComplete(true);
+        }}
+      >
+        Join Meeting
+      </Button>
+    </div>
+  );
 };
 
 export default MeetingSetup;
