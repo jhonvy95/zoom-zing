792dac852e573f7c9693c26662ec56f8ddd518c2
jhon varon <jhonvaronyate@hotmail.com>
2024-05-22 12:49:35 -0500

    Improved the user interface and logic for the home and recordings sections. Updated several components for better styling and functionality. Introduced new constants for the home page. Added new components for listing calls and displaying meeting cards, as well as a custom hook for fetching call data.

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -78,28 +78,27 @@ const MeetingTypeList = () => {
         title="New Meeting"
         description="Start an instant meeting"
         handleClick={() => setMeetingState("isInstantMeeting")}
-        className="bg-orange-1"
-      />
-      <HomeCard
-        img="/icons/schedule.svg"
-        title="schedule Meeting"
-        description="Plan your meeting"
-        handleClick={() => setMeetingState("isScheduleMeeting")}
-        className="bg-blue-1"
-      />
-      <HomeCard
-        img="/icons/recordings.svg"
-        title="View Recordings"
-        description="Check out your recordings"
-        handleClick={() => setMeetingState("isJoiningMeeting")}
-        className="bg-purple-1"
       />
       <HomeCard
         img="/icons/join-meeting.svg"
         title="Join Meeting"
         description="via invitation link"
+        className="bg-blue-1"
         handleClick={() => setMeetingState("isJoiningMeeting")}
+      />
+      <HomeCard
+        img="/icons/schedule.svg"
+        title="Schedule Meeting"
+        description="Plan your meeting"
+        className="bg-purple-1"
+        handleClick={() => setMeetingState("isScheduleMeeting")}
+      />
+      <HomeCard
+        img="/icons/recordings.svg"
+        title="View Recordings"
+        description="Meeting Recordings"
         className="bg-yellow-1"
+        handleClick={() => router.push("/recordings")}
       />
 
       {!callDetails ? (
